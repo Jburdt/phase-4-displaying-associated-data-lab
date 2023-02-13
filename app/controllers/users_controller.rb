22054c21168ecfcb8e5b44b0bb08bc7615b5a899
Josh Burd <Jburdt@gmail.com>
2023-02-13 15:14:23 -0500

    commit tree 444bc236761b72f48f5cce17f67d1e159d081a9a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def show 
+    @users = find_user
+    render json: @users, include: :items
+  end
+  
+  private
+  def find_user
+    User.all.find(params[:id])
+  end
+
 end
